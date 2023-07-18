d7c14491a67b923ae13784f5c76b92a23dff4cab
setareh10 <64446064+setareh10@users.noreply.github.com>
2023-07-18 13:24:50 +0100

    Update - adding deep network

--- compute_timelagged_mdpc.py
+++ compute_timelagged_mdpc.py
@@ -97,6 +97,51 @@ def compute_bv_nonlinear_timelagged_mdpc(x, y):
     return goodness_of_fit
 
 
+def compute_bv_deep_nonlinear_timelagged_mdpc(x, y):
+
+    goodness_of_fit = {}
+    gof_ev_L = np.zeros([y.shape[-1], x.shape[-1]])
+    gof_ev_NL = np.zeros([y.shape[-1], x.shape[-1]])
+
+    for t1 in np.arange(y.shape[-1]):
+        for t2 in np.arange(x.shape[-1]):
+
+            n_trials = x.shape[0]
+            n_splits = 5
+            if (n_trials / 5) > 10:
+                n_splits = 10
+                
+            x_scaled = scale_patterns(x[:, :, t2])
+            y_scaled = scale_patterns(y[:, :, t1])
+            
+            x_cluster = cluster_vertices(x_scaled.transpose())
+            y_cluster = cluster_vertices(y_scaled.transpose())
+            
+            ni = x_cluster.shape[1]
+            no = y_cluster.shape[1]
+            h = int(np.floor((ni + no) / 2))
+            
+            units_l1 = np.random.randint(min(ni,no), max(ni,no))
+            nn = [np.random.randint(min(ni,no), max(ni,no)) for _ in range(3)]
+            nl = [0,1,2,3]
+            activation = ['relu', 'tanh']
+            optimizer= ['sgd', 'Adam']
+            
+            
+            mdpc = MDPC(n_splits,  normalize=False,  alphas=np.logspace(-3, 3, 5), 
+                 scoring="explained_variance", n_jobs=1)
+            
+            bv_mdpc = BiVariateMDPC(mdpc, x_cluster, y_cluster)
+
+            gof_ev_L[t1, t2] = bv_mdpc.bv_nonlinear(hidden_layer_sizes=h, activation_function='identity')
+            gof_ev_NL[t1, t2] = bv_mdpc.bv_deep_nonlinear(units_l1, nl, nn, activation, optimizer)
+
+
+    goodness_of_fit["EV_L"] = gof_ev_L
+    goodness_of_fit["EV_NL"] = gof_ev_NL
+
+    return goodness_of_fit
+
 
 def compute_mv_linear_timelagged_mdpc(output):
 
